adb669f216910aa45dbac553199fff8020374cb9
chris-anderson67 <chrisja67@gmail.com>
2016-03-21 15:38:35 -0400

    Forking tutorial project to attempt real project

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask.ext.login import LoginManager
+
+lm = LoginManager()
+lm.init_app(app)
 
 app = Flask(__name__)
-from app import views
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+from app import views, models
