946b3088105e30f32a19b627c40c612a26407a75
chris-anderson67 <chrisja67@gmail.com>
2016-05-14 15:50:37 -0500

    removed debuggin (broken) from db_upgrade

--- db_upgrade.py
+++ db_upgrade.py
@@ -3,8 +3,8 @@ from migrate.versioning import api
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 from app import app
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
+# app.logger.addHandler(logging.StreamHandler(sys.stdout))
+# app.logger.setLevel(logging.ERROR)
 
 api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
